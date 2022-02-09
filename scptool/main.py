eaa5844ac4158c64a51d010bbf7333a2a9ff6491
Dustin Whited <dgwhited@gmail.com>
2022-02-09 15:19:16 -0600

    init scp code

---
+++ scptool/main.py
@@ -0,0 +1,26 @@
+"""SCPtool
+Usage:
+    scptool [validate | merge] --sourcefiles sourcefiles [ --outdir outdir] [--keep-sids]
+Options:
+    -h --help                   Show this screen.
+    --version                   Show version.
+    --sourcefiles sourcefiles   Directory path to SCP files in json format
+    --outdir outdir             Directory to write new SCP files
+    --keep-sids                 Keeps Sids for metadata purposes
+"""
+from docopt import docopt
+from lib.validate import validate_policies
+from lib.merge import scp_merge
+
+def main():
+    arguments = {
+        k.lstrip('-'): v for k, v in docopt(__doc__, version='SCPtool v0.01').items()
+    }
+    if arguments.get("validate"):
+        validate_policies(**arguments)
+    elif arguments.get("merge"):
+        scp_merge(**arguments)
+
+
+if __name__ == '__main__':
+    main()
